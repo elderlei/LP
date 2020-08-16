157574cdf40d617c1732093151dd6fe50bbaaf1e
Elderlei Souza <68159394+elderlei@users.noreply.github.com>
2020-08-16 17:02:07 -0300

    Create uri1013.c
    O maior

---
+++ uri1013.c
@@ -0,0 +1,23 @@
+//O maior
+
+#include <stdio.h>
+#include <stdlib.h>
+int maior(int,int);
+int main(void) {
+  int a,b,c,res;
+
+  scanf("%d %d %d",&a,&b,&c);
+
+  
+  res=maior(maior(a,b),c);
+
+  printf("%d eh o maior\n",res);
+
+  return 0;
+}
+
+int maior(int a,int b){
+  int res;
+  res=(a+b+abs(a-b))/2;
+  return (res);
+}
