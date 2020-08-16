30f050179bebb6cdc6ff6924c0226ee699ed4023
Elderlei Souza <68159394+elderlei@users.noreply.github.com>
2020-08-16 16:58:19 -0300

    Create uri1051.c
    Imposto de renda

---
+++ uri1051.c
@@ -0,0 +1,29 @@
+//Imposto de renda
+
+#include <stdio.h>
+int main(void){
+  float imposto,total;
+
+  scanf("%f",&total);
+  imposto=0;
+  if (total>0){
+    if (total<2000.000){
+      imposto=0;
+      printf("Isento\n");//O URI está com erro de correção
+    }
+    else if (3000.000>=total){
+      imposto=(total-2000.00)*(0.08);
+      printf("R$ %.2f\n",imposto);
+    }
+    else if(4500.000>=total){
+      imposto=(3000.00-2000.00)*0.08+(total-3000.00)*(0.18);
+  printf("R$ %.2f\n",imposto);
+    }
+    else if(total>4500.000){
+      imposto=(3000.000-2000.00)*0.08+(4500.000-3000.00)*(0.18)+(total-4500.00)*(0.28);
+      printf("R$ %.2f\n",imposto);
+    }
+  }
+  
+  return (0);
+}
