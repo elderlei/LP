2f1abca7b53bb3c895d26627b102b27b02c7ba75
Elderlei Souza <68159394+elderlei@users.noreply.github.com>
2020-10-09 12:15:56 -0300

    Update uri-1035.c
    //Uri 1035: Teste de seleção 1

--- uri-1035.c
+++ uri-1035.c
@@ -1,3 +1,10 @@
+//Uri 1035: Teste de seleção 1
+
+// Se B for maior do que C e se D for maior do que A, e a soma de C com D
+// for maior que a soma de A e B e se C e D, ambos, forem positivos e se
+// a variável A for par, escrever a mensagem "Valores aceitos".
+// Senão, escrever "Valores nao aceitos"
+
 #include <stdio.h>
 
 int main(void) {
