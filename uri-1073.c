92b6c4167ce12c7858c3f63c0fa80e60a98cc9dd
Elderlei Souza <68159394+elderlei@users.noreply.github.com>
2020-10-09 10:18:05 -0300

    Create uri-1073.c

---
+++ uri-1073.c
@@ -0,0 +1,19 @@
+//Uri 1073: Par ou impar
+#include <stdio.h>
+
+int main(void) {
+  long int valor;
+  int i,n;
+
+  scanf("%d",&n);
+
+  for(i=0;i<n;i++){
+    scanf("%ld",&valor);
+    if (!(valor%2)&&(valor!=0)) printf("EVEN");
+    if ((valor%2)) printf("ODD");
+    if ((valor>0)) printf(" POSITIVE\n");
+    if ((valor<0)) printf(" NEGATIVE\n");
+    if (valor==0) printf("NULL\n");
+  }  
+  return 0;
+}
