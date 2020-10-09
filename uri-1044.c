9a4acc88159f50f10c5aceff281774925b3c51c0
Elderlei Souza <68159394+elderlei@users.noreply.github.com>
2020-10-09 11:12:50 -0300

    Create uri-1044.c

---
+++ uri-1044.c
@@ -0,0 +1,13 @@
+// Uri 1044: Multiplos
+
+#include <stdio.h>
+
+int main(void) {
+  int num1,num2;
+
+  scanf("%d %d",&num1,&num2);
+
+  if(num1%num2==0 || num2%num1==0) printf("Sao Multiplos\n");
+  else printf("Nao sao Multiplos\n");
+  return 0;
+}
